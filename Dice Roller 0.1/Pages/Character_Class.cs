e634aebe1a351753c58e240b3e8ce54e5166952b
Isaac Hay <isaac.hay@stu.daviess.kyschools.us>
2023-09-21 13:15:55 -0500

    End of Thursday

--- Dice Roller 0.1/Pages/Character_Class.cs
+++ Dice Roller 0.1/Pages/Character_Class.cs
@@ -2,21 +2,26 @@
 
 public class Character
 {
-    public string name;
-    public int[] stats = new int[6];
+    //Stats and Modifiers
+    //index: 0 is strength, 1 is dexterity, 2 is constitution
+    //index: 3 is intelligence, 4 is wisdom, 5 is charisma
+    public int[] stats = { 10, 10, 10, 10, 10, 10 };
+    public int[] mods = { 0, 0, 0, 0, 0, 0 };
 
-    public Character()
-    {
-        name = "bob";
-        for (int i = 0; i < 6; i++)
-        {
-            stats[i] = 10;
-        }
-    }
+     /*index: (18 skills)
+     * 0 - Acrobatics, 1 - Animal Handling, 2 - Arcana
+     * 3 - Athletics, 4 - Deception, 5 - History
+     * 6 - Insight, 7 - Intimidation, 8 - Investigation
+     * 9 - Medicine, 10 - Nature, 11 - Perception
+     * 12 - Performance, 13 - Persuasion, 14 - Religion
+     * 15 - Sleight of Hand, 16 - Stealth, 17 - Survival
+     */   
+    public int[] skills = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    public Character(string newName, int[] stats)
+
+    public Character(int[] stats)
     {
-        name = newName;
+        
         for (int i = 0;  i < 6; i++)
         {
             if (stats[i] > 30 || stats[i] < 1)
@@ -29,5 +34,15 @@ public class Character
             }
 
         }
+
+        for(int i=0; i<6; i++)
+        {
+            mods[i] = (stats[i] - 10) / 2;
+        }
+    }
+
+    public int getStr()
+    {
+        return 0;
     }
 }
