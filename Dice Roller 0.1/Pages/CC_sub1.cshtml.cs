6ecd7ca0281aef1d19dbf0f58b8ae5ab1f936129
Carter Leohr <carter.leohr@stu.daviess.kyschools.us>
2024-03-18 13:17:41 -0500

    proficiencies

---
+++ Dice Roller 0.1/Pages/CC_sub1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dice_Roller_0._1.Pages
+{
+    public class CC_sub1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Dice Roller 0.1/wwwroot/js/site.js
+++ Dice Roller 0.1/wwwroot/js/site.js
@@ -3,6 +3,28 @@
 
 // Write your JavaScript code.
 
-function testFunction() {
-    alert("hello world, but from javascript");
+function fourDropLowest() {
+    //rolls stats for you and automatically drops the lowest number
+    let low = 9999;
+    let hold, sum;
+
+    //generates the stats
+    for (let i = 0; i < 4; i++) {
+        hold = Math.floor(Math.random() * 6) + 1;
+
+        sum += hold;
+
+        //finds the lowest and stores the value
+        if (hold < low)
+            low = hold;
+    }
+
+    //drops the lowest
+    sum -= low;
+
+    return sum;
+}
+
+function bruh() {
+    return "hello";
 }
