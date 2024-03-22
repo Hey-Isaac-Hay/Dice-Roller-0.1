9c6a123a4e2880a6cacb0c417ac09ebf4a663857
Carter Leohr <carter.leohr@stu.daviess.kyschools.us>
2024-03-22 12:46:19 -0500

    proficiencies

---
+++ Dice Roller 0.1/Pages/CCItems.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dice_Roller_0._1.Pages
+{
+    public class CCItemsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Dice Roller 0.1/wwwroot/js/site.js
+++ Dice Roller 0.1/wwwroot/js/site.js
@@ -2,6 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+/*
+    class Character(){
+
+    }
+*/
 
 function fourDropLowest() {
     //rolls stats for you and automatically drops the lowest number
@@ -25,6 +30,38 @@ function fourDropLowest() {
     return sum;
 }
 
-function bruh() {
-    return "hello";
+
+//This is if we ever go back to js cookies
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        alert("Welcome again " + user);
+    } else {
+        user = prompt("Please enter your name:", "");
+        if (user != "" && user != null) {
+            setCookie("username", user, 365);
+        }
+    }
 }
